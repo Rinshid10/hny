e13f12bb5c596e92279723b0f8eee3efed38cc78
Rinshid10 <rinshidch10@gmail.com>
2025-03-03 15:40:50 +0530

    newone added

---
+++ lib/extone.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class newone extends StatelessWidget {
+  const newone({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 100,
+      width: 100,
+      color: Colors.red,
+    );
+  }
+}
